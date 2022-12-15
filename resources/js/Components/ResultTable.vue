67e9d12cbdbf47f1cf7a3c9b644410d127ab394c
shige0309 <qqa27yed@chic.ocn.ne.jp>
2022-12-15 18:39:43 +0900

    rfm

--- resources/js/Components/ResultTable.vue
+++ resources/js/Components/ResultTable.vue
@@ -42,4 +42,53 @@ const props = defineProps({
             </tbody>
         </table>
     </div>
+    <div v-if="data.type === 'rfm'" class="lg:w-2/3 w-full mx-auto overflow-auto">
+        <!-- 合計人数 -->
+        <h2 class="text-center text-2xl mt-5 mb-2">合計人数</h2>
+        <div class="my-4 text-center font-bold">合計：{{ data.totals }}</div>
+        <!-- RFMランク -->
+        <h2 class="text-center text-2xl mt-5 mb-2">RFMランク</h2>
+        <table class="table-auto w-full text-left whitespace-no-wrap">
+            <thead>
+                <tr>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Rank</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">R</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">F</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">M</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="rfm in data.eachCount" :key="rfm.rank">
+                    <td class="px-4 py-3">{{ rfm.rank }}</td>
+                    <td class="px-4 py-3">{{ rfm.r }}</td>
+                    <td class="px-4 py-3">{{ rfm.f }}</td>
+                    <td class="px-4 py-3">{{ rfm.m }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <!-- RとFの集計 -->
+        <h2 class="text-center text-2xl mt-5 mb-2">RとFの集計</h2>
+        <table class="table-auto w-full text-left whitespace-no-wrap">
+            <thead>
+                <tr>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">rRank</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">f_5</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">f_4</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">f_3</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">f_2</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">f_1</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="rf in data.data" :key="rf.rank">
+                    <td class="px-4 py-3">{{ rf.rRank }}</td>
+                    <td class="px-4 py-3">{{ rf.f_5 }}</td>
+                    <td class="px-4 py-3">{{ rf.f_4 }}</td>
+                    <td class="px-4 py-3">{{ rf.f_3 }}</td>
+                    <td class="px-4 py-3">{{ rf.f_2 }}</td>
+                    <td class="px-4 py-3">{{ rf.f_1 }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
